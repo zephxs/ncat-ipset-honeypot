608adbf3d042c5c4c046f517fe07863117c29df6
zephxs <zephxs@gmail.com>
2020-12-06 22:24:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if ! command -v ipset &> /dev/null; then echo "ipset could not be found" && exit
 if ! command -v ncat &> /dev/null; then echo "ncat could not be found" && exit 1; fi
 
 # IPTables create chain "blocklists"
-Iptables -N blocklists
+iptables -N blocklists
 # make chain in 1st position :
 # delete if needed : iptables -D INPUT -j blocklists
 for _CHX in INPUT OUTPUT FORWARD; do
@@ -17,6 +17,11 @@ ipset create honeypot ip:hash
 # add ipset to the blocklists chain :
 iptables -A blocklists -m set --match-set honeypot src -j DROP
 iptables -A blocklists -m set --match-set honeypot dst -j DROP
+# or add all ipset lists to newly created chain
+for _BLX in $(ipset list -n); do 
+iptables -A blocklists -m set --match-set $_BLX src -j DROP
+iptables -A blocklists -m set --match-set $_BLX dst -j DROP
+done
 
 # copy scripts to path
 mkdir /root/blacklist
